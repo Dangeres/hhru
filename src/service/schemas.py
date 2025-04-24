ecc7d567412e1cc97d0d58943cc8c4721d8176bb
Dmitriy <dimon4ik_228@bk.ru>
2025-04-24 19:41:51 +0300

    added schemas

---
+++ src/service/schemas.py
@@ -0,0 +1,34 @@
+from enum import Enum
+import string
+from typing import Dict
+from pydantic import BaseModel, Field
+
+
+class Resume(BaseModel):
+    title: str = Field(description="Название резюме")
+    href: str = Field(description="Токен для ссылки на резюме")
+    is_active: bool = Field(description="Поднято ли оно в поиске")
+
+
+class MethodEnum(Enum):
+    head = "head"
+    post = "post"
+    get = "get"
+
+
+class Tokens(BaseModel):
+    xsrf: str = Field()
+    hhtoken: str = Field()
+
+
+class Config(BaseModel):
+    username: str = Field(description="Логин авторизации")
+    password: str = Field(description="Пароль авторизации")
+    folder_tokens: str = Field(default="tokens", description="Токены сессии")
+    url: str = Field(default=str("https://hh.ru/"), description="Урлина сайта hh")
+    count_requests: int = Field(
+        default=10,
+        description="Количество попыток сделать запрос",
+    )
+    verify_ssl: bool = Field(default=False)
+    proxy: Dict[str, str] | None = Field(default=None)
